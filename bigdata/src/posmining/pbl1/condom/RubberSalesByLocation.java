48daaa006d6b1e23d5e9f85070c9f5a3b824ab81
tatsuro1992 <tatsuworks@gmail.com>
2014-10-13 18:19:08 +0900

    20141013 立地別、年齢別コンドーム売上個数の集計完了

---
+++ bigdata/src/posmining/pbl1/condom/RubberSalesByLocation.java
@@ -0,0 +1,111 @@
+package posmining.enshu;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import posmining.utils.CSKV;
+import posmining.utils.PosUtils;
+
+public class RubberSalesByLocation {
+	// MapReduceを実行するためのドライバ
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+		// MapperクラスとReducerクラスを指定
+		Job job = new Job(new Configuration());
+		job.setJarByClass(RubberSalesByLocation.class);       // ★このファイルのメインクラスの名前
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		job.setJobName("2014036");                   // ★自分の学籍番号
+
+		// 入出力フォーマットをテキストに指定
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// MapperとReducerの出力の型を指定
+		job.setMapOutputKeyClass(CSKV.class);
+		job.setMapOutputValueClass(CSKV.class);
+		job.setOutputKeyClass(CSKV.class);
+		job.setOutputValueClass(CSKV.class);
+
+		// 入出力ファイルを指定
+		String inputpath = "posdata";
+		String outputpath = "out/RubberSalesByLocation";     // ★MRの出力先
+		if (args.length > 0) {
+			inputpath = args[0];
+		}
+
+		FileInputFormat.setInputPaths(job, new Path(inputpath));
+		FileOutputFormat.setOutputPath(job, new Path(outputpath));
+
+		// 出力フォルダは実行の度に毎回削除する（上書きエラーが出るため）
+		PosUtils.deleteOutputDir(outputpath);
+
+		// Reducerで使う計算機数を指定
+		job.setNumReduceTasks(8);
+
+		// MapReduceジョブを投げ，終わるまで待つ．
+		job.waitForCompletion(true);
+	}
+
+
+	// Mapperクラスのmap関数を定義
+	public static class MyMapper extends Mapper<LongWritable, Text, CSKV, CSKV> {
+		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			// csvファイルをカンマで分割して，配列に格納する
+			String csv[] = value.toString().split(",");
+
+			// コンドームでないレシートは無視
+			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals("コンドーム・スキン") == false) {
+				return;
+			}
+
+			String location = "";
+			// 立地を取得
+			if (csv[PosUtils.LOCATION].equals("オフィス街1")) {
+				location = "1";
+			} else if (csv[PosUtils.LOCATION].equals("駅前1")) {
+				location = "2";
+			} else if (csv[PosUtils.LOCATION].equals("ロードサイド1")) {
+				location = "3";
+			} else if (csv[PosUtils.LOCATION].equals("住宅街1")) {
+				location = "4";
+			} else {
+				location = "Bad Status";
+			}
+
+			// valueとなる販売個数を取得
+			String count = csv[PosUtils.ITEM_COUNT];
+
+			// emitする （emitデータはCSKVオブジェクトに変換すること）
+			context.write(new CSKV(location), new CSKV(count));
+		}
+	}
+
+
+	// Reducerクラスのreduce関数を定義
+	public static class MyReducer extends Reducer<CSKV, CSKV, CSKV, CSKV> {
+		protected void reduce(CSKV key, Iterable<CSKV> values, Context context) throws IOException, InterruptedException {
+
+			// 売り上げを合計
+			int count = 0;
+			for (CSKV value : values) {
+				count += value.toInt();
+			}
+
+			// emit
+			context.write(key, new CSKV(count));
+		}
+	}
+}
